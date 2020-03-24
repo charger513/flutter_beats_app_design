1d6904c6a04b006eb8c83d59f5044021cc894b59
Emmanuel Chargoy <charger513@hotmail.com>
2020-03-24 14:08:25 -0600

    Beats App - Parte 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_beats_app_design/src/pages/home_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,10 +7,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
+      title: 'Beats App',
       initialRoute: 'home',
       routes: {
-
+        'home': (BuildContext context) => HomePage()
       }
     );
   }
